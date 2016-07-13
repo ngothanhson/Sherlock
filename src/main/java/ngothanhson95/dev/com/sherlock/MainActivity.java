75be4a8ec7b44e7517787c4f41c63998ba0506ee
ngothanhson <ngothanhson95@gmail.com>
2016-07-13 15:52:27 +0700

    Initial commit

---
+++ src/main/java/ngothanhson95/dev/com/sherlock/MainActivity.java
@@ -0,0 +1,13 @@
+package ngothanhson95.dev.com.sherlock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
